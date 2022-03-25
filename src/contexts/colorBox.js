a82ebce9f4a47ac83e18e728935d4c89ce62ddf7
salangdung-i <dladmswl92@naver.com>
2022-03-25 15:02:40 +0900

    useContext 사용하기

--- src/contexts/colorBox.js
+++ src/contexts/colorBox.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ColorContext from './color';
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorContext.Consumer >
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: '64px',
-              height: '64px',
-              background: state.color
-            }}
-          />
-          <div
-            style={{
-              width: '32px',
-              height: '32px',
-              background: state.subcolor
-            }}
-          />
-        </>
-      )}
-    </ColorContext.Consumer>
+    <>
+      <div
+        style={{
+          width: '64px',
+          height: '64px',
+          background: state.color
+        }}
+      />
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subcolor
+        }}
+      />
+    </>
   );
 }
 
